e1c226658d98606f7884d062554f2ad240dfec64
nonino <pedrononino@hotmail.com>
2021-06-01 13:30:51 -0300

    osc console

--- Oscilador/Oscilador.cpp
+++ Oscilador/Oscilador.cpp
@@ -21,9 +21,84 @@ public:
 
         Osc = sin(phase);
 
-            std::cout << Osc << std::endl;
+       // std::cout << Osc << std::endl;
+
 
     };
+
+    void Draw() {
+
+        if (Osc > -1 && Osc < -0.9)
+            std::cout << "O-------------------" << std::endl;
+
+
+        else  if (Osc > -0.9 && Osc < -0.8)
+            std::cout << "-O------------------" << std::endl;
+
+        else if (Osc > -0.8 && Osc < -0.7)
+            std::cout << "--O-----------------" << std::endl;
+
+
+        else  if (Osc > -0.7 && Osc < -0.6)
+            std::cout << "---O----------------" << std::endl;
+
+
+        else if (Osc > -0.6 && Osc < -0.5)
+            std::cout << "----O---------------" << std::endl;
+
+
+        else  if (Osc > -0.5 && Osc < -0.4)
+            std::cout << "-----O--------------" << std::endl;
+
+        else if (Osc > -0.4 && Osc < -0.3)
+            std::cout << "------O-------------" << std::endl;
+
+
+        else  if (Osc > -0.3 && Osc < -0.2)
+            std::cout << "-------O------------" << std::endl;
+
+
+        else if (Osc > -0.2 && Osc < -0.1)
+            std::cout << "--------O-----------" << std::endl;
+
+
+        else  if (Osc > -0.1 && Osc < 0)
+            std::cout << "---------O----------" << std::endl;
+
+        else if (Osc > 0 && Osc < 0.1)
+            std::cout << "----------O---------" << std::endl;
+
+
+        else  if (Osc > 0.1 && Osc < 0.2)
+            std::cout << "-----------O---------" << std::endl;
+
+        else  if (Osc > 0.2 && Osc < 0.3)
+            std::cout << "------------O--------" << std::endl;
+
+        else  if (Osc > 0.3 && Osc < 0.4)
+            std::cout << "-------------O-------" << std::endl;
+
+        else  if (Osc > 0.4 && Osc < 0.5)
+            std::cout << "--------------O------" << std::endl;
+
+        else  if (Osc > 0.5 && Osc < 0.6)
+            std::cout << "---------------O-----" << std::endl;
+
+        else  if (Osc > 0.6 && Osc < 0.7)
+            std::cout << "----------------O----" << std::endl;
+
+        else  if (Osc > 0.7 && Osc < 0.8)
+            std::cout << "-----------------O---" << std::endl;
+
+        else  if (Osc > 0.8 && Osc < 0.9)
+            std::cout << "------------------O--" << std::endl;
+
+        else  if (Osc > 0.9 && Osc < 1)
+            std::cout << "-------------------O-" << std::endl;
+
+      
+
+    }
 
 };
 
@@ -35,7 +110,10 @@ int main()
 
     while (1) {
 
-        osc1.Oscilar(100.f);
+        osc1.Oscilar(1000000);
+
+        osc1.Draw();
+
 
 
     }
